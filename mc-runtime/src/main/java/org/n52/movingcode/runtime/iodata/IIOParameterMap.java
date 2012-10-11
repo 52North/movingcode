197659a15cb2d1d18d69f1f68e5d77c9153b5c5e
MatthiasMueller <MatthiasMueller@15ebfbcb-1bbe-4f7e-88aa-3b0fa5d666c2>
2012-10-11 10:39:58 +0000

    code transfer

---
+++ mc-runtime/src/main/java/org/n52/movingcode/runtime/iodata/IIOParameterMap.java
@@ -0,0 +1,53 @@
+package org.n52.movingcode.runtime.iodata;
+
+import java.util.SortedMap;
+
+import org.n52.movingcode.runtime.iodata.IIOParameter.ParameterID;
+
+public interface IIOParameterMap extends SortedMap<ParameterID, IOParameter>{
+	
+	/**
+	 * Method for adding data to this map using the ParameterID.
+	 * (The {@link ParameterID} is the *internal* ID of the parameter)
+	 * 
+	 * 
+	 * @param identifier
+	 * @param data
+	 * @return <code>true</code> if adding was successful, <code>false</code> otherwise
+	 */
+	public boolean addData(ParameterID identifier, Object data);
+	
+	/**
+	 * Method for removing data from this map using the ParameterID.
+	 * (The {@link ParameterID} is the *internal* ID of the parameter)
+	 * 
+	 * 
+	 * @param identifier
+	 * @param data
+	 * @return <code>true</code> if remove was successful, <code>false</code> otherwise
+	 */
+	public boolean removeData(ParameterID identifier, Object data);
+	
+	/**
+	 * Method for adding data to this map using the message ID.
+	 * (The message ID public ID of the parameter from the functional description)
+	 * 
+	 * 
+	 * @param {@link String} identifier
+	 * @param data
+	 * @return <code>true</code> if adding was successful, <code>false</code> otherwise
+	 */
+	public boolean addData(String messageID, Object data);
+	
+	/**
+	 * Method for removing data from this map using the message ID.
+	 * (The message ID public ID of the parameter from the functional description)
+	 * 
+	 * 
+	 * @param {@link String} identifier
+	 * @param data
+	 * @return <code>true</code> if remove was successful, <code>false</code> otherwise
+	 */
+	public boolean removeData(String messageID, Object data);
+	
+}
---
+++ mc-runtime/src/main/java/org/n52/movingcode/runtime/processors/IPlatformComponentProbe.java
@@ -0,0 +1,14 @@
+package org.n52.movingcode.runtime.processors;
+
+/**
+ * A platform probe shall identify the runtime platform based on
+ * some magic tests. 
+ * 
+ * TODO: needs further work
+ * 
+ * @author Matthias Mueller
+ *
+ */
+public interface IPlatformComponentProbe {
+	public String probe();
+}
