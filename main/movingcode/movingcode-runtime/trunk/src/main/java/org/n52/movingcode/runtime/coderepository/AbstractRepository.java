6849410370b48f97848e0e9219ec9f868c50a058
MatthiasMueller <MatthiasMueller@15ebfbcb-1bbe-4f7e-88aa-3b0fa5d666c2>
2013-03-22 14:55:41 +0000

    - added some methods for package retrieval to IMovingCodeRepository - started work on conversion methods for the different repo types

--- main/movingcode/movingcode-runtime/trunk/src/main/java/org/n52/movingcode/runtime/coderepository/AbstractRepository.java
+++ main/movingcode/movingcode-runtime/trunk/src/main/java/org/n52/movingcode/runtime/coderepository/AbstractRepository.java
@@ -1,5 +1,6 @@
 package org.n52.movingcode.runtime.coderepository;
 
+import java.util.Collection;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -14,19 +15,19 @@ import de.tudresden.gis.geoprocessing.movingcode.schema.PackageDescriptionDocume
 
 
 /**
- * Basic class for a package repository. May store multiple packages per functional ID (i.e. multiple
- * implementations of the same functional contract). When requested by their functional ID, it will return
- * only the first registered package (this is the "default"). There is no guarantee about the internal order
+ * Basic class for a package repository. May store multiple packages per function (i.e. multiple
+ * implementations of the same functional contract). When requested by their functionID, it will return
+ * only the first applicable package (this is the "default"). There is no guarantee about the internal order
  * of the registered packages so the default package is an arbitrary item. If you need guaranteed access to a
- * particular implementation you have to request that package by its *packageID*.
+ * particular implementation you have to request that package by its packageID.
  * 
- * TODO: unify/split/re-arrange with {@link MovingCodeRepository}  
  * 
  * @author Matthias Mueller, TU Dresden
  * 
  * 
  */
 public abstract class AbstractRepository implements IMovingCodeRepository{
+	
 	// registered packages - KVP (packageID, mPackage)
 	private Map<String, MovingCodePackage> packages = new HashMap<String, MovingCodePackage>();
 	
@@ -37,7 +38,7 @@ public abstract class AbstractRepository implements IMovingCodeRepository{
 	
 	/**
 	 * Private method for registering packages with this repository instance.
-	 * Puts a new KVP (packageID, mPackage) into the packages Map.
+	 * Puts a new KVP (packageID, mcPackage) into the packages Map.
 	 * Also registers a KVP (functionalID,packageID) in the fIDpID_Lookup.
 	 * 
 	 * @param mcPackage {@link MovingCodePackage}
@@ -46,70 +47,49 @@ public abstract class AbstractRepository implements IMovingCodeRepository{
 		this.packages.put(mcPackage.getPackageIdentifier(), mcPackage);
 		this.fIDpID_Lookup.put(mcPackage.getFunctionalIdentifier(), mcPackage.getPackageIdentifier());
 	}
-	
-	/**
-	 * Protected method for package retrieval. Returns a package for a given ID.
-	 * 
-	 * @param packageID {@link String} - the internal (unique) identifier of package.
-	 * @return a package {@link MovingCodePackage}
-	 */
-	protected MovingCodePackage retrievePackage(String packageID) {
-		return packages.get(packageID);
+
+	@Override
+	public boolean providesFunction(String functionID) {
+		return fIDpID_Lookup.containsKey(functionID);
 	}
 
-	/**
-	 * Public method to determine whether this repository instance provides a certain functionality
-	 * (i.e. ProcessIdentifier in WPS 1.0). 
-	 * 
-	 * @param functionalID {@link String}
-	 * @return boolean - returns true if this repository instance contains a suitable package for a given functional ID, false otherwise.
-	 */
-	public boolean providesFunction(String functionalID) {
-		return fIDpID_Lookup.containsKey(functionalID);
-	}
-
-	/**
-	 * Public method to determine whether a package with the given ID is provided by this repository?
-	 * 
-	 * @param packageID {@link String} - the internal (unique) identifier of package. 
-	 * @return boolean - true if a package with the given ID is provided by this repository.
-	 */
+	@Override
 	public boolean containsPackage(String packageID) {
 		return packages.containsKey(packageID);
 	}
 
-	/**
-	 * Returns the *functional* IDs of all registered packages
-	 * 
-	 * Returns a snapshot of all currently used identifiers. Later changes in the MovingCodeRepository (new or
-	 * deleted packages) are not propagated to the returned array. If you need up-to-date information call
-	 * this method again.
-	 */
-	public String[] getFunctionalIDs() {
+	@Override
+	public String[] getFunctionIDs() {
 		return fIDpID_Lookup.keySet().toArray(new String[fIDpID_Lookup.keySet().size()]);
 	}
 
-	/**
-	 * Returns the *package* IDs of all registered packages.
-	 * 
-	 * @return Array of packageIDs {@link String}
-	 */
+	@Override
 	public String[] getPackageIDs() {
 		return packages.keySet().toArray(new String[packages.keySet().size()]);
 	}
 	
     @Override
     public MovingCodePackage getPackage(String packageID) {
-        return retrievePackage(packageID);
+    	return packages.get(packageID);
     }
 
     @Override
     public Date getPackageTimestamp(String packageID) {
-        return retrievePackage(packageID).getTimestamp();
+    	return packages.get(packageID).getTimestamp();
     }
 
     @Override
     public PackageDescriptionDocument getPackageDescription(String packageID) {
-        return retrievePackage(packageID).getDescription();
+    	return packages.get(packageID).getDescription();
+    }
+    
+    @Override
+    public MovingCodePackage getPackageByFunction(String functionID){
+    	Collection<String> packageIDs = fIDpID_Lookup.get(functionID);
+    	if (packageIDs.size() > 0){
+    		return packages.get(packageIDs.iterator().next());
+    	} else {
+    		return null;
+    	}
     }
 }
---
+++ main/movingcode/movingcode-runtime/trunk/src/main/java/org/n52/movingcode/runtime/coderepository/RepositoryUtils.java
@@ -0,0 +1,78 @@
+package org.n52.movingcode.runtime.coderepository;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+
+/**
+ * This class contains static utility methods to convert between different
+ * {@link IMovingCodeRepository} implementations.
+ * 
+ * @author Matthias Mueller
+ *
+ */
+public class RepositoryUtils {
+	
+	/**
+	 * Conversion method that creates a new {@link LocalZipPackageRepository} repository from any given {@link IMovingCodeRepository}.
+	 * Automatically attempts to create the targetDirectory if it does not exist.
+	 * 
+	 * @param sourceRepo {@link IMovingCodeRepository} - source repository to copy the content from
+	 * @param targetDirectory {@link File} - blank(!) target directory that shall be used to create the new repository
+	 * @return {@link LocalZipPackageRepository}
+	 * @throws IOException - throws an exception in case the repository cannot be created
+	 */
+	public static final LocalZipPackageRepository materializeAsLocalZipRepo(IMovingCodeRepository sourceRepo, File targetDirectory) throws IOException{
+		
+		// check if directory exists, create if necessary
+		if (!targetDirectory.exists()){
+			targetDirectory.createNewFile();
+		}
+		
+		// check if directory is empty
+		if (targetDirectory.list().length > 0){
+			throw new IOException("Cannot create repository. Target directory is not empty.");
+		}
+		
+		// get contents of old repository
+		String[] sourcePackageIDs = sourceRepo.getPackageIDs();
+		
+		// analyze IDs
+		
+		// 1. guess common root
+		
+		// 2. guess folder structure (based on PackageID String)
+		
+		/**
+		 * http://stackoverflow.com/questions/1005551/construct-a-tree-structure-from-list-of-string-paths
+		 */
+		
+		// or:
+		
+		// simply split up the ID at path separators
+		
+		// HashMap<sourcePackageID, targetFolder>
+		HashMap<String, File> targetFolders = new HashMap<String, File>();
+		
+		// HashMap<sourcePackageID, targetPackageID> (latter is a short ID)
+		HashMap<String, String> targetIDs = new HashMap<String, String>();
+		
+		
+		
+		// for each package:
+		// add to the new folder
+		for (String currentSourceID : sourcePackageIDs){
+			File targetFolder = targetFolders.get(currentSourceID);
+			if (!targetFolder.exists()){
+				targetFolder.createNewFile();
+			}
+			
+			File zipFile = new File(targetFolder, targetIDs.get(currentSourceID) + ".zip");
+			// TODO: dump package to zip
+		}
+		
+		// return new Repo for the folder
+		return new LocalZipPackageRepository(targetDirectory);
+	}
+	
+}
