ba203b8e06bacb62540e454e068824b81469558a
MatthesRieke <MatthesRieke@15ebfbcb-1bbe-4f7e-88aa-3b0fa5d666c2>
2013-04-04 12:02:51 +0000

    continue: restructure of the project. xmlbeans bindings and mc-runtime now in separated modules.

---
+++ mc-runtime/src/test/java/org/n52/movingcode/runtime/test/ProbeTest.java
@@ -0,0 +1,60 @@
+/**
+ * ﻿Copyright (C) 2012
+ * by 52 North Initiative for Geospatial Open Source Software GmbH
+ *
+ * Contact: Andreas Wytzisk
+ * 52 North Initiative for Geospatial Open Source Software GmbH
+ * Martin-Luther-King-Weg 24
+ * 48155 Muenster, Germany
+ * info@52north.org
+ *
+ * This program is free software; you can redistribute and/or modify it under
+ * the terms of the GNU General Public License version 2 as published by the
+ * Free Software Foundation.
+ *
+ * This program is distributed WITHOUT ANY WARRANTY; even without the implied
+ * WARRANTY OF MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program (see gnu-gpl v2.txt). If not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA or
+ * visit the Free Software Foundation web page, http://www.fsf.org.
+ */
+
+package org.n52.movingcode.runtime.test;
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+import org.n52.movingcode.runtime.processors.python.PythonCLIProbe;
+import org.n52.movingcode.runtime.processors.r.RCLIProbe;
+import org.n52.movingcode.runtime.processors.r.R_RServeProbe;
+
+public class ProbeTest extends GlobalTestConfig {
+
+    private static Logger log = Logger.getLogger(ProbeTest.class);
+
+    @Test
+    public void probePythonCLI() {
+        PythonCLIProbe p = new PythonCLIProbe();
+        String retVal = p.probe();
+
+        log.info("Python Probe reported: " + retVal);
+    }
+
+    @Test
+    public void probeRCLI() {
+        RCLIProbe p = new RCLIProbe();
+        String retVal = p.probe();
+
+        log.info("R CLI Probe reported: " + retVal);
+    }
+
+    @Test
+    public void probeR_RServe() {
+        R_RServeProbe p = new R_RServeProbe();
+        String retVal = p.probe();
+
+        log.info("R RServe Probe reported: " + retVal);
+    }
+}
