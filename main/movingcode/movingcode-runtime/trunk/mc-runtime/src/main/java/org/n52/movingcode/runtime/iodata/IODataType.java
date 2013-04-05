50d69dd17a950ba3b90498580591dabc09eb0165
MatthiasMueller <MatthiasMueller@15ebfbcb-1bbe-4f7e-88aa-3b0fa5d666c2>
2013-04-05 11:50:27 +0000

    added documentation / future tasks for data type lookup and conversion

--- main/movingcode/movingcode-runtime/trunk/mc-runtime/src/main/java/org/n52/movingcode/runtime/iodata/IODataType.java
+++ main/movingcode/movingcode-runtime/trunk/mc-runtime/src/main/java/org/n52/movingcode/runtime/iodata/IODataType.java
@@ -40,95 +40,104 @@ import net.opengis.wps.x100.OutputDescriptionType;
  * 
  */
 public enum IODataType {
-    STRING(String.class), INTEGER(Integer.class), DOUBLE(Double.class), BOOLEAN(Boolean.class), MEDIA(MediaData.class);
+	STRING(String.class), INTEGER(Integer.class), DOUBLE(Double.class), BOOLEAN(Boolean.class), MEDIA(MediaData.class);
 
-    private final Class< ? extends Object> clazz;
+	private final Class< ? extends Object> clazz;
 
-    IODataType(Class< ? extends Object> clazz) {
-        this.clazz = clazz;
-    }
+	IODataType(Class< ? extends Object> clazz) {
+		this.clazz = clazz;
+	}
 
-    /**
-     * Returns the Java Class associated with a particular IODataType.
-     * 
-     * @return Class<? extends Object> - the Java Class
-     */
-    public Class< ? extends Object> getSupportedClass() {
-        return this.clazz;
-    }
+	/**
+	 * Returns the Java Class associated with a particular IODataType.
+	 * Currently performs educated guesses rather than performing a lookup
+	 * in XML type systems.
+	 * 
+	 * @return Class<? extends Object> - the Java Class
+	 */
+	public Class< ? extends Object> getSupportedClass() {
+		return this.clazz;
+	}
 
-    public static final IODataType findDataType(DomainMetadataType wpsLiteral) throws UnknownFormatConversionException {
+	public static final IODataType findDataType(DomainMetadataType wpsLiteral) throws UnknownFormatConversionException {
 
-        // check possible well-known data types
-        String datatype = wpsLiteral.getStringValue();
-        if (datatype == null || datatype.trim().isEmpty()) {
-        	if (wpsLiteral.isSetReference()) {
-        		datatype = wpsLiteral.getReference().trim();
-        		String[] split = datatype.split(":");
-        		if (split != null && split.length > 1) {
-        			datatype = split[split.length-1];
-        		}
-        	}
-        }
+		// 1. try it with well-known data types in the OGC type system
+		String datatype = wpsLiteral.getStringValue();
 
-        if (datatype.equalsIgnoreCase("string")) {
-            return IODataType.STRING;
-        }
-        if (datatype.equalsIgnoreCase("boolean")) {
-            return IODataType.BOOLEAN;
-        }
-        if (datatype.equalsIgnoreCase("float")) {
-            return IODataType.DOUBLE;
-        }
-        if (datatype.equalsIgnoreCase("double")) {
-            return IODataType.DOUBLE;
-        }
-        if (datatype.equalsIgnoreCase("int")) {
-            return IODataType.INTEGER;
-        }
-        if (datatype.equalsIgnoreCase("integer")) {
-            return IODataType.INTEGER;
-        }
+		// 2. try it with external type systems
+		// TODO: might need more intelligence for educated data
+		// type guesses
+		if (datatype == null || datatype.trim().isEmpty()) {
+			if (wpsLiteral.isSetReference()) {
 
-        return null;
-    }
+				// Current procedure works well for xml schema data types
+				// of the form [ns]:[string|boolean|float|double|int|integer]
+				datatype = wpsLiteral.getReference().trim();
+				String[] split = datatype.split(":");
+				if (split != null && split.length > 1) {
+					datatype = split[split.length-1];
+				}
+			}
+		}
 
-    public static IODataType findType(InputDescriptionType wpsInput) {
-        // return null for null
-        if (wpsInput == null) {
-            return null;
-        }
+		if (datatype.equalsIgnoreCase("string")) {
+			return IODataType.STRING;
+		}
+		if (datatype.equalsIgnoreCase("boolean")) {
+			return IODataType.BOOLEAN;
+		}
+		if (datatype.equalsIgnoreCase("float")) {
+			return IODataType.DOUBLE;
+		}
+		if (datatype.equalsIgnoreCase("double")) {
+			return IODataType.DOUBLE;
+		}
+		if (datatype.equalsIgnoreCase("int")) {
+			return IODataType.INTEGER;
+		}
+		if (datatype.equalsIgnoreCase("integer")) {
+			return IODataType.INTEGER;
+		}
 
-        // assign mimeType and genericDataType
-        if (wpsInput.isSetComplexData()) {
-            return IODataType.MEDIA;
-        }
+		return null;
+	}
 
-        if (wpsInput.isSetBoundingBoxData()) {
-            // TODO not implemented
-        }
-        if (wpsInput.isSetLiteralData()) {
-            return findDataType(wpsInput.getLiteralData().getDataType());
-        }
-        return null; // should not occur, but who knows ...
-    }
+	public static IODataType findType(InputDescriptionType wpsInput) {
+		// return null for null
+		if (wpsInput == null) {
+			return null;
+		}
 
-    public static IODataType findType(OutputDescriptionType wpsOutput) {
-        // return null for null
-        if (wpsOutput == null) {
-            return null;
-        }
+		// assign mimeType and genericDataType
+		if (wpsInput.isSetComplexData()) {
+			return IODataType.MEDIA;
+		}
 
-        // assign mimeType and genericDataType
-        if (wpsOutput.isSetComplexOutput()) {
-            return IODataType.MEDIA;
-        }
-        if (wpsOutput.isSetBoundingBoxOutput()) {
-            // TODO not implemented
-        }
-        if (wpsOutput.isSetLiteralOutput()) {
-            return findDataType(wpsOutput.getLiteralOutput().getDataType());
-        }
-        return null; // should not occur, but who knows ...
-    }
+		if (wpsInput.isSetBoundingBoxData()) {
+			// TODO not implemented
+		}
+		if (wpsInput.isSetLiteralData()) {
+			return findDataType(wpsInput.getLiteralData().getDataType());
+		}
+		return null; // should not occur, but who knows ...
+	}
+
+	public static IODataType findType(OutputDescriptionType wpsOutput) {
+		// return null for null
+		if (wpsOutput == null) {
+			return null;
+		}
+
+		// assign mimeType and genericDataType
+		if (wpsOutput.isSetComplexOutput()) {
+			return IODataType.MEDIA;
+		}
+		if (wpsOutput.isSetBoundingBoxOutput()) {
+			// TODO not implemented
+		}
+		if (wpsOutput.isSetLiteralOutput()) {
+			return findDataType(wpsOutput.getLiteralOutput().getDataType());
+		}
+		return null; // should not occur, but who knows ...
+	}
 }
