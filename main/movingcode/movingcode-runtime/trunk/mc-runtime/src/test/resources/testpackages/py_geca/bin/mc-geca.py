c1e96079a1e6fcb13e650cc5f3a156b611a5d487
MatthesRieke <MatthesRieke@15ebfbcb-1bbe-4f7e-88aa-3b0fa5d666c2>
2013-04-08 09:48:53 +0000

    improve geca python script. includes now a config file to set the base dataset folder and an optional zip output.

--- main/movingcode/movingcode-runtime/trunk/mc-runtime/src/test/resources/testpackages/py_geca/bin/mc-geca.py
+++ main/movingcode/movingcode-runtime/trunk/mc-runtime/src/test/resources/testpackages/py_geca/bin/mc-geca.py
@@ -4,18 +4,24 @@ import fileinput, os
 import sys, getopt
 import shutil
 import uuid
+import ConfigParser
+from contextlib import closing
+from zipfile import ZipFile, ZIP_DEFLATED
 
 TEMPLATE_FILE = "template_definition.xml"
 WORKSPACE_SUB_FOLDER = "workspace"
 
-def copyAndPrepareTemplateDefinition(cwd, arguments):
+def copyAndPrepareTemplateDefinition(cwd, arguments, dataset_base):
+	print dataset_base
 	tmpDef = uuid.uuid4().hex +".xml"
 	
 	shutil.copyfile(TEMPLATE_FILE, tmpDef)
 	for line in fileinput.input(tmpDef, inplace = 1): # Does a list of files, and writes redirects STDOUT to the file in question
 		newLine = line.replace("${workspace}", cwd + os.sep + WORKSPACE_SUB_FOLDER)
-		newLine = newLine.replace("${satellite_a}", arguments[1])
-		newLine = newLine.replace("${satellite_b}", arguments[2])
+		newLine = newLine.replace("${dataset_a_base_folder}", dataset_base + os.sep + arguments[1])
+		newLine = newLine.replace("${dataset_b_base_folder}", dataset_base + os.sep + arguments[2])
+		newLine = newLine.replace("${dataset_a}", arguments[1])
+		newLine = newLine.replace("${dataset_b}", arguments[2])
 		newLine = newLine.replace("${CollocationCriteria_dt}", arguments[3])
 		newLine = newLine.replace("${CollocationCriteria_dp}", arguments[4])
 		newLine = newLine.replace("${ResamplingScheme}", arguments[5])
@@ -24,31 +30,50 @@ def copyAndPrepareTemplateDefinition(cwd, arguments):
 		
 	return tmpDef
 
-def copyResultToOutputFile(pdfDest):
-	shutil.copy(WORKSPACE_SUB_FOLDER + os.sep + "output/report.pdf", pdfDest)
+def zipdir(basedir, archivename):
+    assert os.path.isdir(basedir)
+    with closing(ZipFile(archivename, "w", ZIP_DEFLATED)) as z:
+        for root, dirs, files in os.walk(basedir):
+            #NOTE: ignore empty directories
+            for fn in files:
+                absfn = os.path.join(root, fn)
+                zfn = absfn[len(basedir)+len(os.sep):] #XXX: relative path
+                z.write(absfn, zfn)
+
+def copyResultToOutputFiles(arguments):
+	dest = arguments[7]
+	shutil.copy(WORKSPACE_SUB_FOLDER + os.sep + "output"+ os.sep +"report.pdf", dest)
+	
+	if (len(arguments) > 8):
+		dest = arguments[8]
+		zipdir(WORKSPACE_SUB_FOLDER + os.sep + "output", dest)
+		
 
 def main(argv):	
 
 	if (len(sys.argv) < 8):
-		print "Usage: python mc-geca.py <satellite_a> <satellite_b> <CollocationCriteria_dt> <CollocationCriteria_dp> <ResamplingScheme> <ResamplingMaster> <pdfOutputFile>"
+		print "Usage: python mc-geca.py <dataset_a> <dataset_b> <CollocationCriteria_dt> <CollocationCriteria_dp> <ResamplingScheme> <ResamplingMaster> <pdfOutputFile> [<zipOutputFile>]"
 		sys.exit()
 
-	for arg in sys.argv:
-		print arg
-	
+	#change the working dir to the scripts folder
 	cwd = os.path.dirname(os.path.realpath(__file__))
 	os.chdir(cwd)
-	print os.getcwd()
-	outputPdfFile = sys.argv[7]
+	print "Working dir: " +os.getcwd()
+
+	#read config
+	config = ConfigParser.ConfigParser()
+	cfgpath = cwd+ os.sep +".."+ os.sep + "geca-config.cfg"
+	print cfgpath
+	config.read(cfgpath)
 	
-	tempDefinition = copyAndPrepareTemplateDefinition(cwd, sys.argv)
+	#prepare the template file
+	tempDefinition = copyAndPrepareTemplateDefinition(cwd, sys.argv, config.get("workspace", "dataset_base_folder"))
 	
 	print "Using temporary definition file: "+ tempDefinition
 	os.system("gtcontrol "+tempDefinition)
 	
-	copyResultToOutputFile(outputPdfFile)
+	#finally, copy output files
+	copyResultToOutputFiles(sys.argv)
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
-
-
