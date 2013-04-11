58c736aaf4830848fbbbe54c6add9a5009d9d1e0
MatthiasMueller <MatthiasMueller@15ebfbcb-1bbe-4f7e-88aa-3b0fa5d666c2>
2013-04-11 13:17:52 +0000

    implemented change listener functions for RemoteFeedRepository

--- main/movingcode/movingcode-runtime/trunk/mc-runtime/src/main/java/org/n52/movingcode/runtime/coderepository/RemoteFeedRepository.java
+++ main/movingcode/movingcode-runtime/trunk/mc-runtime/src/main/java/org/n52/movingcode/runtime/coderepository/RemoteFeedRepository.java
@@ -27,6 +27,9 @@ package org.n52.movingcode.runtime.coderepository;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import org.apache.abdera.model.Entry;
 import org.apache.log4j.Logger;
@@ -35,68 +38,142 @@ import org.n52.movingcode.runtime.feed.GeoprocessingFeed;
 import org.n52.movingcode.runtime.feed.GeoprocessingFeedEntry;
 
 /**
- * This class implements an {@link IMovingCodeRepository} for Remote Geoprocessing Feeds
+ * This class implements an {@link IMovingCodeRepository} for Remote Geoprocessing Feeds.
+ * 
+ * Performs occasional checks for updated content.
+ * (Interval for periodical checks is given by {@link IMovingCodeRepository#remotePollingInterval})
  * 
  * @author Matthias Mueller, TU Dresden
  * 
  */
 public final class RemoteFeedRepository extends AbstractRepository {
 
-    static Logger logger = Logger.getLogger(RemoteFeedRepository.class);
+	static Logger logger = Logger.getLogger(RemoteFeedRepository.class);
+	
+	// Atom Feed URL
+	private final URL atomFeedURL;
+	
+	// Last known update of the feed
+	private Date lastFeedUpdate;
+	
+	// Timer for content updates
+	private final Timer timerDaemon;
+	
+	
 
-    /**
-     * 
-     * Constructor for atom feed repositories. Tries to access the atom feed at the given URL and scans its
-     * entries. Then attempts to interpret the entries as MovingCodePackages. Packages that do not validate
-     * will be ignored
-     * 
-     * @param atomFeedURL
-     *        {@link URL} - Direct HTTP link to the Geoprocessing Feed.
-     * 
-     */
-    public RemoteFeedRepository(final URL atomFeedURL) {
-        InputStream stream = null;
-        try {
-            logger.debug("Create RemoteFeedRepository from " + atomFeedURL);
-            stream = atomFeedURL.openStream();
-            GeoprocessingFeed feed = new GeoprocessingFeed(stream);
+	/**
+	 * 
+	 * Constructor for atom feed repositories. Tries to access the atom feed at the given URL and scans its
+	 * entries. Then attempts to interpret the entries as MovingCodePackages. Packages that do not validate
+	 * will be ignored
+	 * 
+	 * @param atomFeedURL
+	 *        {@link URL} - Direct HTTP link to the Geoprocessing Feed.
+	 * 
+	 */
+	public RemoteFeedRepository(final URL atomFeedURL) {
+		this.atomFeedURL = atomFeedURL;
+		load();
+		// start timer daemon
+		timerDaemon = new Timer(true);
+		timerDaemon.scheduleAtFixedRate(new CheckFeed(), 0, IMovingCodeRepository.remotePollingInterval);
+	}
+	
+	private void load(){
+		InputStream stream = null;
+		try {
+			logger.debug("Create RemoteFeedRepository from " + atomFeedURL);
+			// TODO: Do it with Apache HTTPClient
+			stream = atomFeedURL.openStream();
+			GeoprocessingFeed feed = new GeoprocessingFeed(stream);
+			lastFeedUpdate = feed.lastUpdated();
 
-            for (Entry entry : feed.getEntries()) {
-                // create new moving code package from the entry
-                GeoprocessingFeedEntry gpfe = new GeoprocessingFeedEntry(entry);
-                logger.trace("Loading entry " + gpfe.toString());
+			for (Entry entry : feed.getEntries()) {
+				// create new moving code package from the entry
+				GeoprocessingFeedEntry gpfe = new GeoprocessingFeedEntry(entry);
+				logger.trace("Loading entry " + gpfe.toString());
 
-                // FIXME this call slows down startup of WPS server
-                MovingCodePackage mcPackage = new MovingCodePackage(gpfe);
+				// FIXME this call slows down startup of WPS server
+				MovingCodePackage mcPackage = new MovingCodePackage(gpfe);
 
-                // validate
-                // and add to package map
-                // and add current file to zipFiles map
-                if (mcPackage.isValid()) {
-                    register(mcPackage);
-                }
-                else {
-                    logger.debug("Info: " + atomFeedURL.toString() + " contains an invalid package: "
-                            + mcPackage.getPackageIdentifier());
-                }
-            }
+				// validate
+				// and add to package map
+				// and add current file to zipFiles map
+				if (mcPackage.isValid()) {
+					register(mcPackage);
+				}
+				else {
+					logger.debug("Info: " + atomFeedURL.toString() + " contains an invalid package: "
+							+ mcPackage.getPackageIdentifier());
+				}
+			}
 
-            stream.close();
-        }
-        catch (IOException e) {
-            System.err.println("Could read feed from URL: " + atomFeedURL);
-        }
-        finally {
-            if (stream != null)
-                try {
-                    stream.close();
-                }
-                catch (IOException e) {
-                    logger.error("Could not close GeoprocessingFeed stream.", e);
-                }
-        }
+			stream.close();
+		}
+		catch (IOException e) {
+			logger.error("Could read feed from URL: " + atomFeedURL);
+		}
+		finally {
+			if (stream != null)
+				try {
+					stream.close();
+				}
+			catch (IOException e) {
+				logger.error("Could not close GeoprocessingFeed stream.", e);
+				stream = null;
+			}
+		}
 
-        logger.trace("Created!");
-    }
+		logger.trace("Created!");
+	}
+	
+	/**
+	 * A task which re-checks the remote feed's last update and
+	 * triggers a content reload if required.
+	 * 
+	 * @author Matthias Mueller
+	 * 
+	 */
+	private final class CheckFeed extends TimerTask {
+		
+		@Override
+		public void run() {
+			InputStream stream = null;
+			try {
+				// TODO: Do it with Apache HTTPClient
+				stream = atomFeedURL.openStream();
+				GeoprocessingFeed feed = new GeoprocessingFeed(stream);
+				
+				// if feed's update time is newer than last known update time
+				// re-init the feed
+				if (feed.lastUpdated().after(lastFeedUpdate)){
+					logger.info("Repository content has silently changed. Running update ...");
+					
+					// clear contents and reload
+					clear();
+					load();
+					
+					logger.info("Reload finished. Calling Repository Change Listeners.");
+					
+					informRepositoryChangeListeners();
+				}
+				stream.close();
+			}
+			catch (IOException e) {
+				logger.error("Could read feed from URL: " + atomFeedURL);
+			}
+			finally {
+				if (stream != null)
+					try {
+						stream.close();
+					}
+				catch (IOException e) {
+					logger.error("Could not close GeoprocessingFeed stream.", e);
+					stream = null;
+				}
+			}		
+		}
+	}
+
 
 }
