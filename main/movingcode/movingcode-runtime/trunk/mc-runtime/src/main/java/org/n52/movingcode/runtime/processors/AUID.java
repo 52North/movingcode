2cba68e012d3fcb85986fa3b2274d5500bd0da61
MatthiasMueller <MatthiasMueller@15ebfbcb-1bbe-4f7e-88aa-3b0fa5d666c2>
2013-04-17 11:32:28 +0000

    Added AUID test case

--- main/movingcode/movingcode-runtime/trunk/mc-runtime/src/main/java/org/n52/movingcode/runtime/processors/AUID.java
+++ main/movingcode/movingcode-runtime/trunk/mc-runtime/src/main/java/org/n52/movingcode/runtime/processors/AUID.java
@@ -27,46 +27,42 @@ package org.n52.movingcode.runtime.processors;
 import java.security.SecureRandom;
 
 /**
- * A Class that creates an Almost Unique ID (AUID).
+ * A Class that creates an Almost Unique ID (AUID). Compared to a UUID, the AUID
+ * is consists of only eight characters which should be sufficiently unique for
+ * random file name generation within this framework.
  * 
  * @author Matthias Mueller, Geoinformation Systems, TU Dresden
  * 
  */
 public class AUID {
-    /*
-     * The random number generator used by this class to create random based AUIDs.
-     */
-    private static volatile SecureRandom numberGenerator = null;
+	/*
+	 * The random number generator used by this class to create random based AUIDs.
+	 */
+	private static volatile SecureRandom numberGenerator = null;
 
-    private static final String alphabetPlusNum = "0123456789abcdefghijklmnopqrstuvwxyz";
-    private static final String alphabet = "abcdefghijklmnopqrstuvwxyz";
+	private static final String alphabetPlusNum = "0123456789abcdefghijklmnopqrstuvwxyz";
+	private static final String alphabet = "abcdefghijklmnopqrstuvwxyz";
 
-    /**
-     * Creates a random AUID
-     * 
-     * @return - an eight digit alphanumeric string starting with a letter
-     */
-    public static String randomAUID() {
-        SecureRandom ng = numberGenerator;
-        if (ng == null) {
-            numberGenerator = ng = new SecureRandom();
-        }
+	/**
+	 * Creates a random AUID
+	 * 
+	 * @return - an eight digit alphanumeric string starting with a letter
+	 */
+	public static String randomAUID() {
+		SecureRandom ng = numberGenerator;
+		if (ng == null) {
+			numberGenerator = ng = new SecureRandom();
+		}
 
-        char[] auid = new char[8];
-        // make the first element a character
-        auid[0] = alphabet.charAt(ng.nextInt(alphabet.length()));
+		char[] auid = new char[8];
+		// make the first element a character
+		auid[0] = alphabet.charAt(ng.nextInt(alphabet.length()));
 
-        // make the other characters alphanumeric
-        for (int i = 1; i < 8; i++) {
-            auid[i] = alphabetPlusNum.charAt(ng.nextInt(alphabet.length()));
-        }
+		// make the other characters alphanumeric
+		for (int i = 1; i < 8; i++) {
+			auid[i] = alphabetPlusNum.charAt(ng.nextInt(alphabet.length()));
+		}
 
-        return new String(auid);
-    }
-
-    // public static void main(String args[]){
-    // for (int i=0; i<100; i++){
-    // System.out.println(randomAUID());
-    // }
-    // }
+		return new String(auid);
+	}
 }
