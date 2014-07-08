7ca2a28903cecfdb8bc008061a4bf768d37878ea
MatthiasMueller <MatthiasMueller@15ebfbcb-1bbe-4f7e-88aa-3b0fa5d666c2>
2014-07-08 13:37:48 +0000

    - added test for cached remote repo

--- mc-runtime/src/test/java/org/n52/movingcode/runtime/test/MovingCodeRepositoryTests.java
+++ mc-runtime/src/test/java/org/n52/movingcode/runtime/test/MovingCodeRepositoryTests.java
@@ -25,17 +25,20 @@
 package org.n52.movingcode.runtime.test;
 
 import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import org.apache.commons.io.FileUtils;
 import org.junit.Assert;
 import org.junit.Test;
-
 import org.n52.movingcode.runtime.GlobalRepositoryManager;
 import org.n52.movingcode.runtime.codepackage.MovingCodePackage;
+import org.n52.movingcode.runtime.codepackage.PID;
 import org.n52.movingcode.runtime.coderepository.IMovingCodeRepository;
 import org.n52.movingcode.runtime.iodata.IOParameter;
 import org.n52.movingcode.runtime.iodata.IOParameterMap;
 import org.n52.movingcode.runtime.processors.AUID;
+import org.n52.movingcode.runtime.processors.AbstractProcessor;
 import org.n52.movingcode.runtime.processors.ProcessorFactory;
 
 import de.tudresden.gis.geoprocessing.movingcode.schema.PackageDescriptionDocument;
@@ -148,6 +151,117 @@ public class MovingCodeRepositoryTests extends MCRuntimeTestConfig {
 		// show report
 		logger.info(report.toString());
 	}
+	
+	@Test
+	public void remoteFeedRepoTest() {
+		// create string buffer for the test report
+		StringBuffer report = new StringBuffer(CR);
+		
+		try {
+			URL url = new URL(MCRuntimeTestConfig.feedURL);
+			IMovingCodeRepository mcRep = IMovingCodeRepository.Factory.createFromRemoteFeed(url);
+			logger.info("Added Repo: " + MCRuntimeTestConfig.feedURL);
+
+			for (PID pID : mcRep.getPackageIDs()) {
+				report.append("\nFound process: " + pID + CR);
+				MovingCodePackage pack = mcRep.getPackage(pID);
+
+				Assert.assertFalse(pack == null); // make sure it is not null
+
+				AbstractProcessor processor = ProcessorFactory.getInstance().newProcessor(pack); // get an
+				// empty
+				// parameter
+				// Map
+				if (processor == null) {
+					report.append("Couldn't get a processor for package " + pID + CR);
+				}
+
+				else {
+
+					report.append("Compatible Processors: " + processor.getClass().getName() + CR);
+
+					report.append("--- Parameters ---" + CR);
+					for (IOParameter param : processor.values()) {
+						report.append("Parameter " + param.getIdentifier().getHarmonizedValue() + ": "
+								+ param.getMinMultiplicity() + ".." + param.getMaxMultiplicity() + CR);
+						if (param.isMessageIn()) {
+							report.append("ServiceInputID: " + param.getMessageInputIdentifier() + CR);
+						}
+						if (param.isMessageOut()) {
+							report.append("ServiceOutputID: " + param.getMessageOutputIdentifier() + CR);
+						}
+
+						report.append("Internal Type: " + param.getType().toString() + CR);
+					}
+				}
+
+			}
+			
+			// show report
+			logger.info(report.toString());
+
+		}
+		catch (MalformedURLException e) {
+			logger.info("Could not read test feed from URL " + feedURL + CR + "Please check if this feed is indeed up and running.");
+		}
+	}
+	
+	@Test
+	public void cachedRemoteFeedRepoTest() {
+		// create string buffer for the test report
+		StringBuffer report = new StringBuffer(CR);
+		
+		try {
+			URL url = new URL(MCRuntimeTestConfig.feedURL);
+			File cacheWS = newTempDir();
+			IMovingCodeRepository mcRep = IMovingCodeRepository.Factory.createCachedRemoteRepository(url, cacheWS);
+			logger.info("Added Repo: " + MCRuntimeTestConfig.feedURL);
+			logger.info("Using Cache directory: " + cacheWS.getAbsolutePath());
+
+			for (PID pID : mcRep.getPackageIDs()) {
+				report.append("\nFound process: " + pID + CR);
+				MovingCodePackage pack = mcRep.getPackage(pID);
+
+				Assert.assertFalse(pack == null); // make sure it is not null
+
+				AbstractProcessor processor = ProcessorFactory.getInstance().newProcessor(pack); // get an
+				// empty
+				// parameter
+				// Map
+				if (processor == null) {
+					report.append("Couldn't get a processor for package " + pID + CR);
+				}
+
+				else {
+
+					report.append("Compatible Processors: " + processor.getClass().getName() + CR);
+
+					report.append("--- Parameters ---" + CR);
+					for (IOParameter param : processor.values()) {
+						report.append("Parameter " + param.getIdentifier().getHarmonizedValue() + ": "
+								+ param.getMinMultiplicity() + ".." + param.getMaxMultiplicity() + CR);
+						if (param.isMessageIn()) {
+							report.append("ServiceInputID: " + param.getMessageInputIdentifier() + CR);
+						}
+						if (param.isMessageOut()) {
+							report.append("ServiceOutputID: " + param.getMessageOutputIdentifier() + CR);
+						}
+
+						report.append("Internal Type: " + param.getType().toString() + CR);
+					}
+				}
+
+			}
+			
+			// show report
+			logger.info(report.toString());
+			
+		}
+		catch (MalformedURLException e) {
+			logger.info("Could not read test feed from URL " + feedURL + CR + "Please check if this feed is indeed up and running.");
+		}
+		
+	}
 
 	@Test
 	public void testRepoManager() {
@@ -191,5 +305,13 @@ public class MovingCodeRepositoryTests extends MCRuntimeTestConfig {
 
 		// Assert
 		Assert.assertTrue(mcp.isValid());
+	}
+	
+	private static final File newTempDir(){
+		File tmpDir = FileUtils.getTempDirectory();
+		File newTmpFolder = new File(tmpDir, AUID.randomAUID());
+		newTmpFolder.mkdir();
+		newTmpFolder.deleteOnExit();
+		return newTmpFolder;
 	}
 }
